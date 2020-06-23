8f301e8462c440baaa292f083ea8e9e15be3d501
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-22 23:36:30 -0400

    Added all required UI elements, their labels and respective String resources

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+
+Name: Francisco Ozuna Diaz
+Assignment: CS 7455 Lab 5
+Lab Date: Due June 28, 2020 at 11:59 PM
+
+ */
+
 package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
