f7bee96244207b51f5276372176868a3bd941cef
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-24 20:56:49 -0400

    Added onClickListeners for all of the required Buttons

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -11,12 +11,59 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button startServiceButton;
+    Button stopServiceButton;
+    Button bindServiceButton;
+    Button unbindServiceButton;
+    Button getRandomNumberButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startServiceButton = (Button) findViewById(R.id.button_startService);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        stopServiceButton = (Button) findViewById(R.id.button_stopService);
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bindServiceButton = (Button) findViewById(R.id.button_bindService);
+        bindServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        unbindServiceButton = (Button) findViewById(R.id.button_unbindService);
+        unbindServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        getRandomNumberButton = (Button) findViewById(R.id.button_getRandomNumber);
+        getRandomNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
