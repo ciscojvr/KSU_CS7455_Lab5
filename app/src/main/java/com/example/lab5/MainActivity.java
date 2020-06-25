598c38ca1b76499b8c5e3c7cb530db513d30cbb0
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-25 01:11:41 -0400

    Minor code cleanup

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -16,7 +16,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
